a5376a9eb14875cfe5ff39541bc39f3b15e7f152
Cindy G <68168256+cindy-531@users.noreply.github.com>
2021-08-28 16:35:53 -0400

    deleted comments

--- src/App.js
+++ src/App.js
@@ -18,25 +18,3 @@ function App() {
 }
 
 export default App;
-
-
-/*import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-
-function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-        </Switch>
-      </Router>
-    </>
-  );
-}
-
-export default App;*/
