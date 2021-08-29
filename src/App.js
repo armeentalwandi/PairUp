3316b652ee0071a2848987a77f8d45798446c750
Armeen Talwandi <armeentalwandi@gmail.com>
2021-08-28 23:55:26 -0400

    create destination route page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import {Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import MapWithADirectionsRenderer from './Map';
+import GoogleMaps from './startingpoint';
+import GoogleMaps1 from './destination';
+
+//import { Button } from '@material-ui/core';
+ 
+import { useState } from 'react';
+
+
+
 function App() {
+//  const [startlat, setStartlat] = useState(43.4723); 
+//  const [endlat, setEndlat] = useState(43.4739); // 3 more 
+//  const [startlong, setStartlong] = useState(-80.5449); 
+//  const [endlong, setEndlong] = useState(-80.5274);
+
+ const [startlat, setStartlat] = useState(); 
+ const [endlat, setEndlat] = useState(); // 3 more 
+ const [startlong, setStartlong] = useState(); 
+ const [endlong, setEndlong] = useState(); 
   return (
+ 
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    
+      <p><Link to="/homepage">Get Directions</Link></p> 
+      <header className="content">
+        <Switch>
+         <Route path= "/homepage">
+         <GoogleMaps setStartlat = {setStartlat} setStartlong = {setStartlong}/>
+         <GoogleMaps1 setEndlat = {setEndlat} setEndlong = {setEndlong}/>
+
+       {/* <Button startlat = {startlat} startlong = {startlong} 
+          endlat={endlat} endlong={endlong} onClick=}> Find Buddies</Button> */}
+
+          <MapWithADirectionsRenderer startlat = {startlat} startlong = {startlong} 
+          endlat={endlat} endlong={endlong}/>
+           
+          </Route>
+          </Switch>
+        </header>
+      </div> 
+
+    </Router>
+   
   );
 }
 
